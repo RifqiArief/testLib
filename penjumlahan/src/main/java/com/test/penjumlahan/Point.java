dff0a564ec5e69197f69f3b0e5e36eb90bc0cb62
Sumringah <rifkiarif46@gmail.com>
2020-11-25 14:11:16 +0700

    nambahin pengurangan

--- penjumlahan/src/main/java/com/test/penjumlahan/Point.java
+++ penjumlahan/src/main/java/com/test/penjumlahan/Point.java
@@ -2,7 +2,11 @@ package com.test.penjumlahan;
 
 public class Point {
 
-    public static int plus(int a, int b){
+    public int plus(int a, int b){
         return a+b;
+    }
+
+    public int minus(int a, int b){
+        return a-b;
     }
 }
